7529b8c36f447f22ef328aaa1e5cac2bec208652
TaerangLee <s22051@gsm.hs.kr>
2024-02-18 05:59:28 +0900

    Add signInPage style

--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { LogoIcon } from "../../assets";
 import { AxiosApi } from "../../libs/AxiosApi";
 import * as S from "./style";
+import { ToastContainer, toast } from "react-toastify";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const SignInPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigation = useNavigate();
 
   console.log(email);
   console.log(password);
@@ -17,12 +21,19 @@ const SignInPage = () => {
           email,
           password,
         });
+
+        localStorage.setItem("accessToken", response.data.accessToken);
+
         console.log("POST request successful:", response.data);
+        navigation("/");
+        toast.success("로그인이 성공하였습니다.");
       } else {
         console.error("Title and content are required");
+        toast.error("로그인에 실패하였습니다.");
       }
     } catch (error) {
       console.error("Error while sending POST request:", error);
+      toast.error("로그인에 실패하였습니다.");
     }
   };
 
@@ -41,6 +52,7 @@ const SignInPage = () => {
             <S.InputTitle
               placeholder="비밀번호를 입력해주세요"
               onChange={(e) => setPassword(e.target.value)}
+              type="password"
             />
           </S.InputContainer>
           <S.NextButton onClick={onCreate}>로그인</S.NextButton>
---
+++ src/pages/signin/style.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const SignInWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SignInContainer = styled.div`
+  width: 28.125rem;
+  height: 31.25rem;
+  /* background-color: white; */
+  border-radius: 1.25rem;
+  padding: 1.875rem;
+`;
+
+export const LogoContainer = styled.div`
+  width: 100%;
+  height: 8.125rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  margin-top: 1.25rem;
+`;
+
+export const InputTitle = styled.input`
+  width: 27.5rem;
+  height: 3.5625rem;
+  border-radius: 1rem;
+  border: none;
+  outline: none;
+  text-indent: 1.125rem;
+  color: white;
+  font-size: 1rem;
+
+  background-color: #2e2e3c;
+
+  &:hover {
+    border: 0.0625rem solid #c8deff;
+  }
+`;
+
+export const NextButton = styled.button`
+  width: 27.5rem;
+  height: 3.5625rem;
+  background-color: #c8deff;
+  border-radius: 1rem;
+  font-size: 1rem;
+  color: black;
+  border: none;
+  font-weight: 700;
+
+  &::placeholder {
+    font-size: 1rem;
+    color: #5c5c61;
+  }
+
+  &:hover {
+    opacity: 90%;
+  }
+
+  &:active {
+    opacity: 70%;
+  }
+`;
