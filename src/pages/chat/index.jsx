600595194800673779a04e44bee9579331107b90
TaerangLee <s22051@gsm.hs.kr>
2024-02-18 05:58:03 +0900

    Add chatPage style

---
+++ src/pages/chat/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import * as S from "./style";
+
+const ChatPage = () => {
+  return (
+    <>
+      <Header />
+      <div>sad</div>
+    </>
+  );
+};
+
+export default ChatPage;
