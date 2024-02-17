eb1c244683ad9eaf7bcb6719fd0a734dd0147d37
TaerangLee <s22051@gsm.hs.kr>
2024-02-18 05:59:35 +0900

    Update setting router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,8 +1,24 @@
 import { Route, Routes } from "react-router-dom";
+import MainPage from "../pages/main";
+import SignInPage from "../pages/signin";
+import SignUpPage from "../pages/signup";
+import NoticePage from "../pages/notice";
+import MyPage from "../pages/my";
+import ChatPage from "../pages/chat";
+
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>asasdd</div>}></Route>
+      <Route path="/" element={<MainPage />}></Route>
+      <Route path="/signin" element={<SignInPage />}></Route>
+      <Route path="/signup" element={<SignUpPage />}></Route>
+      <Route path="/notice" element={<NoticePage />}></Route>
+      <Route path="/my" element={<MyPage />}></Route>
+      <Route path="/chat" element={<ChatPage />}></Route>
+
+      {/* <Route path="/signup" element={<MainPage />}></Route>
+      <Route path="/notice" element={<MainPage />}></Route>
+      <Route path="/chat" element={<MainPage />}></Route> */}
     </Routes>
   );
 };
