a5773d84f1a6e74349bf9b1dc8275adeb47cf0f0
TaerangLee <s22051@gsm.hs.kr>
2024-02-18 05:57:03 +0900

    Add export Icons

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as HomeIcon } from "./HomeIcon";
+export { default as MyIcon } from "./MyIcon";
+export { default as ChatIcon } from "./ChatIcon";
+export { default as NoticeIcon } from "./NoticeIcon";
+export { default as LogoIcon } from "./LogoIcon";
