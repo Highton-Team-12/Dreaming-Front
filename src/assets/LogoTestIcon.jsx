2fbb4635443c8cb8b65cc46cc525f9504e6bc721
TaerangLee <s22051@gsm.hs.kr>
2024-02-17 21:11:19 +0900

    Update svg import

---
+++ src/assets/LogoTestIcon.jsx
@@ -0,0 +1,15 @@
+const LogoTestIcon = () => {
+  return (
+    <svg
+      width="5.125rem"
+      height="5.125rem"
+      viewBox="0 0 82 82"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="82" height="82" fill="white" />
+    </svg>
+  );
+};
+
+export default LogoTestIcon;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import * as S from "./style";
+import LogoTestIcon from "../../assets/LogoTestIcon";
+
+const Header = () => {
+  return (
+    <S.HeaderWrapper>
+      <S.HeaderContainer></S.HeaderContainer>
+    </S.HeaderWrapper>
+  );
+};
+
+export default Header;
