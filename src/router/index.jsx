4ace294d8b3710a7dc20b0fb4452439520dd8a33
TaerangLee <s22051@gsm.hs.kr>
2024-02-17 18:51:21 +0900

    Update setting router

---
+++ src/router/index.jsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/sign" element={<div>asasdd</div>}></Route>
+    </Routes>
+  );
+};
+
+export default Router;
