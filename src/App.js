5f0e6e48864eba691834dcf80363e3c3a7a57127
TaerangLee <s22051@gsm.hs.kr>
2024-02-18 05:58:51 +0900

    Update toastift alert setting, style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Router from "./router";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <div
+      style={{ backgroundColor: "#11111a", width: "100vw", height: "100vh" }}
+    >
+      <BrowserRouter>
+        <ToastContainer />
+        <Router />
+      </BrowserRouter>
+    </div>
   );
 }
 
